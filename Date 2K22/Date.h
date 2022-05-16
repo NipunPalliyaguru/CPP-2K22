8a47ab9e7f2aa65e890ed152ce7bf7cf48098ba4
Nipun Palliyaguru <99266866+IT21187278@users.noreply.github.com>
2022-05-16 13:28:02 +0530

    Create Date.h

---
+++ Date 2K22/Date.h
@@ -0,0 +1,18 @@
+//Date.h file
+
+class Date{
+
+private:
+  int day;
+  int month;
+  int year;
+
+public:
+  void setDay(int d);
+  void setMonth(int m);
+  void setYear(int y);
+  int getDay();
+  int getMonth();
+  int getYear();
+
+};
